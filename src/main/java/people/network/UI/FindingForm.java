88c316b7b08fd58c6aa82fabe9cc5d9e252f55c7
gregmazur <gregorymazurr@gmail.com>
2016-03-17 18:22:42 +0200

    cities countries

--- src/main/java/people/network/UI/FindingForm.java
+++ src/main/java/people/network/UI/FindingForm.java
@@ -1,9 +1,9 @@
 package people.network.UI;
 
-import com.vaadin.data.Property;
 import com.vaadin.ui.*;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
+import people.network.UI.utils.InfoRole;
 import people.network.entity.ResponseSearchCriteriaObj;
 import people.network.rest.JsonService;
 import people.network.rest.Utils;
@@ -17,8 +17,14 @@ public class FindingForm extends VerticalLayout {
 
     private JsonService service;
     private MultiValueMap<String, String> userSearchParams = new LinkedMultiValueMap<>(35);
+    ComboBox homeCountry = new ComboBox("Home country"), homeCity = new ComboBox("Home city"),
+            currentCountry = new ComboBox("Current country"), currentCity = new ComboBox("Current city"),
+            universityCountry = new ComboBox("University country"), universityCity = new ComboBox("University city");
+
     private Button submit;
     private TextArea result;
+
+
 
     public FindingForm(JsonService service) {
         super();
@@ -50,53 +56,88 @@ public class FindingForm extends VerticalLayout {
     }
 
     private VerticalLayout homeInfo(Collection<ResponseSearchCriteriaObj> countries) {
-        ComboBox homeCountry = new ComboBox("Home country"), homeCity = new ComboBox("Home city");
-        return fillCityCountry(countries, homeCountry, homeCity, "school_country");
+        InfoRole role = InfoRole.HOME;
+        return fillCityCountry(countries, role);
     }
 
     private VerticalLayout currentInfo(Collection<ResponseSearchCriteriaObj> countries) {
-        ComboBox currentCountry = new ComboBox("Current country"), currentCity = new ComboBox("Current city");
-        return fillCityCountry(countries, currentCountry, currentCity, "city");
+        InfoRole role = InfoRole.CURRENT;
+        return fillCityCountry(countries, role);
     }
 
     private VerticalLayout universityInfo(Collection<ResponseSearchCriteriaObj> countries) {
-        ComboBox universityCountry = new ComboBox("University country"), universityCity = new ComboBox("University city");
-        return fillCityCountry(countries, universityCountry, universityCity,
-                "university_country");
+        InfoRole role = InfoRole.UNIVERSITY;
+        return fillCityCountry(countries, role);
     }
 
-    private VerticalLayout fillCityCountry(Collection<ResponseSearchCriteriaObj> countries, ComboBox countryCB, ComboBox cityCB,
-                                           String countryParam) {
+    private VerticalLayout fillCityCountry(Collection<ResponseSearchCriteriaObj> countries, InfoRole role) {
         VerticalLayout layout = new VerticalLayout();
         layout.setWidth(30, Unit.PERCENTAGE);
-        countryCB.setWidth(80, Unit.PERCENTAGE);
+        ComboBox countryCB = bindCountry(role);
+        ComboBox cityCB = bindCity(role);
+        countryCB.setWidth(30, Unit.PERCENTAGE);
+        countryCB.setImmediate(true);
         Utils.bindItemsToComboBox(countryCB, countries);
-        final ComboBox finalCountryCB = countryCB;
-        final ComboBox finalCityCB = cityCB;
+
         countryCB.addValueChangeListener(event -> {
-            finalCityCB.setVisible(true);
-            ResponseSearchCriteriaObj o = (ResponseSearchCriteriaObj) finalCountryCB.getValue();
-            userSearchParams.add(countryParam, String.valueOf(o.getId()));
-            loadCities(finalCityCB, o.getId());
+            cityCB.setVisible(true);
+            ResponseSearchCriteriaObj o = (ResponseSearchCriteriaObj) countryCB.getValue();
+            cityCB.removeAllItems();
+            loadCities(cityCB, o.getId(), null, 0);
+            cityCB.setValue(null);
         });
         layout.addComponent(countryCB);
+        cityCB.setImmediate(true);
+        cityCB.addValueChangeListener(event -> {
 
+        });
+        cityCB.setNewItemsAllowed(true);
+        cityCB.setNewItemHandler(newItemCaption -> {
+            cityCB.removeAllItems();
+            ResponseSearchCriteriaObj o = (ResponseSearchCriteriaObj) countryCB.getValue();
+            loadCities(cityCB, o.getId(), newItemCaption, 0);
+        });
         cityCB.setVisible(false);
         layout.addComponent(cityCB);
         return layout;
     }
 
+    private ComboBox bindCountry(InfoRole role) {
+        ComboBox country = null;
+        if (role == InfoRole.HOME) {
+            country = homeCountry;
+        } else if (role == InfoRole.UNIVERSITY) {
+            country = universityCountry;
+        } else if (role == InfoRole.CURRENT) {
+            country = currentCountry;
+        }
+        return country;
+    }
+
+    private ComboBox bindCity(InfoRole role) {
+        ComboBox city = null;
+        if (role == InfoRole.HOME) {
+            city = homeCity;
+        } else if (role == InfoRole.UNIVERSITY) {
+            city = universityCity;
+        } else if (role == InfoRole.CURRENT) {
+            city = currentCity;
+        }
+        return city;
+    }
+
     private Collection<ResponseSearchCriteriaObj> getCountriesList() {
         MultiValueMap<String, String> map = new LinkedMultiValueMap<>(4);
         map.add("need_all", "1");
-        return service.getCriteriaList(Utils.GET_COUNTRIES_METHOD, map);
+        return service.getCriteriaList(Utils.GET_COUNTRIES_METHOD, map, 1000, 0);
     }
 
-    private void loadCities(ComboBox comboBox, int country) {
+    private void loadCities(ComboBox comboBox, int country, String name, int from) {
         MultiValueMap<String, String> map = new LinkedMultiValueMap<>(4);
+        if (null != name) map.add("q", name);
         map.add("need_all", "1");
         map.add("country_id", String.valueOf(country));
-        Collection<ResponseSearchCriteriaObj> items = service.getCriteriaList(Utils.GET_CITIES_METHOD, map);
+        Collection<ResponseSearchCriteriaObj> items = service.getCriteriaList(Utils.GET_CITIES_METHOD, map, 1000, from);
         Utils.bindItemsToComboBox(comboBox, items);
     }
 
---
+++ src/main/java/people/network/UI/utils/InfoRole.java
@@ -0,0 +1,5 @@
+package people.network.UI.utils;
+
+public enum InfoRole {
+    HOME, UNIVERSITY, CURRENT
+}
