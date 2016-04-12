4e1c90a5bd6c9916ef45604942d0989e69c38504
ArtemYemelin <artem.yemelin@gmail.com>
2016-04-12 11:51:20 +0300

    Commit

--- src/main/java/people/network/PeopleFinderApplication.java
+++ src/main/java/people/network/PeopleFinderApplication.java
@@ -6,6 +6,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import people.network.service.utils.AppProperties;
 
 import java.io.IOException;
 
@@ -13,16 +14,11 @@ import java.io.IOException;
 @SpringBootApplication
 public class PeopleFinderApplication {
 
-
 	public static void main(String[] args) throws IOException {
-
 		SpringApplication.run(PeopleFinderApplication.class, args);
-
-		//for IBIS
-		System.setProperty("https.proxyHost", "proxy.ibis");
-		System.setProperty("https.proxyPort", "3128");
+		if(AppProperties.isProxyInUse()) {
+			System.setProperty("https.proxyHost", AppProperties.getProxyHost());
+			System.setProperty("https.proxyPort", AppProperties.getProxyPort());
+		}
 	}
-
-
-
 }
